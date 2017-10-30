2e50163e9919e54a3aa6767c59db6906683b38ea
go.ugotsta.com <ugotsta@gmail.com>
2017-10-30 14:58:43 -0700

    Simplified messaging event.

--- js/script.js
+++ js/script.js
@@ -406,25 +406,22 @@ jQuery(document).ready(function () {
         }
     }
 
-    // wait for Ready message from open windows
-    function receiveMessage(event) {
-        //console.log('Data received: ' + event.data);
-        if ( event.origin === "https://ugotsta.github.io" ) {
-            if ( event.data === 'Ready.' ) {
-                var content = export_content();
-                $(eid).append('<div id="gd-export"></div>');
-                content = $('#gd-export').html(content).text();
-                $('#gd-export').remove();
-                event.source.postMessage( content, "*" );
-                console.log('Message sent to child window.');
-            }
-        }
-    }
-
     function register_events() {
 
         // listen for Ready messages from any opened windows
-        window.addEventListener( 'message', receiveMessage, false );
+        window.addEventListener( 'message', function(event) {
+            //console.log('Data received: ' + event.data);
+            if ( event.origin === "https://ugotsta.github.io" ) {
+                if ( event.data === 'Ready.' ) {
+                    var content = export_content();
+                    $(eid).append('<div id="gd-export"></div>');
+                    content = $('#gd-export').html(content).text();
+                    $('#gd-export').remove();
+                    event.source.postMessage( content, "*" );
+                    console.log('Message sent to child window.');
+                }
+            }
+        }, false);
 
         $(eid + ' .info .field.selector.app a.id').click(function (e) {
             var url = $(this).attr('data-id');
